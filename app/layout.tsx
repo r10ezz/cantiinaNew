460150b0d052045f3057cc28c704a497edb0c151
Raj Singh <knotr10@gmail.com>
2025-10-06 13:29:17 +0000

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ import { Nav } from "@/components/cantiina/nav"
 import { Suspense } from "react"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Cantiina",
+  description: "Demo for cantiina",
+  generator: "create react app",
 }
 
 export default function RootLayout({
